ac61062b666d3e0f20db282365dcc0455308cc76
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-06-07 18:21:19 +0300

    reduct App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+// import React, { Component } from 'react';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const setElement = e => {
+    // const { good, neutral, bad } = e.target;
+
+    switch (e) {
+      case 'good':
+        setGood(good + 1);
+        break;
+
+      case 'bad':
+        setBad(bad + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+
+      default:
+        console.warn(`There is somesing wrong`);
+    }
   };
 
-  setElement = element => {
-    this.setState(prevState => ({ [element]: prevState[element] + 1 }));
-  };
+  const total = good + neutral + bad;
+  const feedbacks = Object.keys({ good, neutral, bad });
+  const countPositiveFeedbackPercentage = Math.round((good / total) * 100);
 
-  countTotalFeedback = () => {
-    const elements = Object.values(this.state);
-    return elements.reduce((total, element) => total + element, 0);
-  };
-
-  render() {
-    const feedbacks = Object.keys(this.state);
-    const total = this.countTotalFeedback();
-    const countPositiveFeedbackPercentage = Math.round(
-      (this.state.good / total) * 100
-    );
-
-    return (
-      <>
-        <Section text="Please leave headback">
-          <FeedbackOptions
-            options={feedbacks}
-            onLeaveFeedback={this.setElement}
+  return (
+    <>
+      <Section text="Please leave headback">
+        <FeedbackOptions options={feedbacks} onLeaveFeedback={setElement} />
+      </Section>
+      {total > 0 ? (
+        <Section text="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
         </Section>
-        {total > 0 ? (
-          <Section text="Statistics">
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={total}
-              positivePercentage={countPositiveFeedbackPercentage}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </>
-    );
-  }
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </>
+  );
 }
